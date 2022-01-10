b7af218b7fe98be976561a1434b79ba5536ce5a6
Jake <jchamblee1995@gmail.com>
2022-01-10 15:50:52 -0600

    1/10/2022

--- main.py
+++ main.py
@@ -55,12 +55,12 @@ def extract_accession(feature: str) -> str:
     return accession
 
 
-def extract_test_seq_position(feature: str) -> str:
+def extract_test_seq_position(feature: str) -> List[str]:
     """Extract position in genome. Gets any number of values '(.*)' between the brackets
-    using 'lookbehind/lookright' (?<=PATTERN) and 'lookahead/lookleft' regex assertions"""
-    # Matches for 'Location=[(.*)]('
+    using 'lookbehind/lookright' (?<=PATTERN) and 'lookahead/lookleft' regex assertions
+    to extract (?<=Location=\\[)(.*)(?=]\\()"""
     location: Match = re.search('(?<=Location=\\[)(.*)(?=]\\()', feature)
-    location: str = location.group().split(':')
+    location: List[str] = location.group().split(':')
 
     return location
 
@@ -86,20 +86,29 @@ def extract_promoter_data(feature: str) -> Dict[str, str]:
     # (the bprom output does not maintain the same # of whitespace characters
     # if there are less digits, at least for the scoring)
     minus10: List[str] = minus10.group().lstrip().split(' ')
-    minus10_pos: str = minus10[0]
+    minus10_pos: int = int(minus10[0])
     minus10_seq: str = minus10[1]
     minus10_score: str = minus10[-1]
 
     minus35: List[str] = minus35.group().lstrip().split(' ')
-    minus35_pos: str = minus35[0]
+    minus35_pos: int = int(minus35[0])
     minus35_seq: str = minus35[1]
     minus35_score: str = minus35[-1]
-    promoter_data = {'minus10_pos': minus10_pos,
-                     'minus10_seq': minus10_seq,
-                     'minus10_score': minus10_score,
-                     'minus35_pos': minus35_pos,
-                     'minus35_seq': minus35_seq,
-                     'minus35_score': minus35_score}
+    promoter_data = {  # 'minus10_pos': minus10_pos,
+        'minus10_seq': minus10_seq,
+        # 'minus10_score': minus10_score,
+        'minus35_pos': minus35_pos,
+        'minus35_seq': minus35_seq,
+        # 'minus35_score': minus35_score
+    }
+    return promoter_data
+
+
+def convert_extracted_promoter_data_to_ID_column_format(promoter_data: Dict[str, str]) -> str:
+    """Converts input data to the GFF3 ID column (column 9) format, a semicolon separated
+       list of values providing additional information about each feature"""
+    promoter_data: List[str] = [f'{key}={value}' for key, value in promoter_data.items()]
+    promoter_data: str = 'Note=' + ';'.join(promoter_data)
     return promoter_data
 
 
@@ -112,41 +121,52 @@ def extract_LDF_score(feature: str) -> str:
 
 
 def extract_promoter_position(feature: str):
-    """Extract promoter position (not -10 or -35 box position)"""
+    """Extract promoter position (not -10 or -35 box position) based on BPROM predictions.
+       Calculates the predicted promoter position in the context of the genome and CDS
+       used to make the prediction."""
     # Get 'Promoter Pos:     X' data
     promoter_pos: Match = re.search('(?<=Promoter Pos:)(.*)(?=LDF)', feature)
     promoter_pos: int = int(promoter_pos.group().strip())
 
     # Get start and end positions from 'Location=[XXX:YYYY]'
-    test_seq_position: List[str] = extract_test_seq_position(feature).split(':')
-    test_seq_start: int = int(test_seq_position[0])
-    test_seq_end: int = int(test_seq_position[1])
+    test_seq_position: List[str] = extract_test_seq_position(feature)
+    test_cds_location_start_pos: int = int(test_seq_position[0])
+    test_cds_location_end_pos: int = int(test_seq_position[1])
 
-    # Now need to calculate promoter position with respect to the genome
-    # Get's -10 position and -35 position
-    # -35 position is the START, Promoter Pos is the end?? Or -10 + 10 is the end??
+    # Get -35 promoter position
+    promoter_data = extract_promoter_data(feature)
+    minus35_pos: int = promoter_data['minus35_pos']
 
-    # IMPORTANT!! Whether or not you add or subtract to calculate the promoter start
-    # position depends on whether we're on the + or - strand! Must take into account.
+    ''' IMPORTANT!! Whether or not you add or subtract to calculate the promoter start
+    # position depends on whether we're on the + or - strand!
+    # The workflow Jolene uses is smart enough to correctly pull upstream
+    # for both + and - strands (i.e., pulls left for +, pulls right for -)
+    # THEREFORE, for a gene with a start at 930 on the + strand, it pulls 830:930
+    # And for a gene with a start at 930 on the - strand, it pulls 930:1030 '''
+
     direction: str = extract_strand_direction(feature)
 
     if direction == '+':
-        # start = test_seq/CDS start + minus35 position
-        # end = start + 35
-        pass
+        # Start = index 0; End = index 1
+        start: int = test_cds_location_start_pos + minus35_pos
+        end: int = start + 35
+        calculated_promoter_pos: List[int] = [start, end]
+        return calculated_promoter_pos
+
     elif direction == '-':
-        # The workflow Jolene uses is smart enough to correctly pull upstream
-        # for both + and - strands (i.e., pulls left for +, pulls right for -)
-        # THEREFORE, for a gene with a start at 930 on the + strand, it pulls 830:930
-        # And for a gene with a start at 930 on the - strand, it pulls 930:1030
-        pass
+        # NEED TO CHECK/FIX THIS. Calculation for - strand may be wrong. #
+        start: int = test_cds_location_end_pos - minus35_pos
+        end: int = start - 35
+        calculated_promoter_pos: List[int] = [start, end]
+        return calculated_promoter_pos
+
     else:
         assert "Error: Strand data neither \'+\' nor \'-\'"
 
     calculated_promoter_pos: List[int] = [
-        promoter_pos + test_seq_end,
-        promoter_pos + test_seq_start,
-                                         ]
+        promoter_pos + test_cds_location_end_pos,
+        promoter_pos + test_cds_location_start_pos,
+    ]
 
     return calculated_promoter_pos
 
@@ -162,23 +182,20 @@ def extract_data_for_all_features(features: List[str]) -> List[List[str]]:
     for feature in features:
         # loop through features, a List[str] containing each feature [str] in the
         # original bprom format as a single string, but cleaned of irrelevant data
-        promoter_data = extract_promoter_data(feature)
-        location: List[str] = extract_test_seq_position(feature)
+        promoter_data: Dict[str, str] = extract_promoter_data(feature)
+        promoter_data_converted: str = convert_extracted_promoter_data_to_ID_column_format(promoter_data)
+
+        promoter_position: List[int] = extract_promoter_position(feature)
         data.append(
-            [extract_accession(feature),            # Seqid col
-             'bprom',                               # Source col
-             'promoter',                            # Type col
-             location[0],                           # Start col
-             location[1],                           # End col
-             extract_strand_direction(feature),
-             extract_LDF_score(feature),
-             extract_promoter_position(feature),
-             promoter_data['minus10_pos'],
-             promoter_data['minus10_seq'],
-             promoter_data['minus10_score'],
-             promoter_data['minus35_pos'],
-             promoter_data['minus35_seq'],
-             promoter_data['minus35_score'],
+            [extract_accession(feature),  # Seqid col (1)
+             'bprom',  # Source col (2)
+             'promoter',  # Type col (3)
+             promoter_position[0],  # Start column (4)
+             promoter_position[1],  # End column (5)
+             extract_LDF_score(feature),  # Score column (6)
+             extract_strand_direction(feature),  # Strand direction column (7)
+             '.',  # Phase column (8) '.' for all
+             promoter_data_converted,  # Attributes column (9)
              ])
 
     return data
@@ -187,21 +204,11 @@ def extract_data_for_all_features(features: List[str]) -> List[List[str]]:
 def convert_to_dataframe(extracted_data: List[List[str]]):
     """Convert extracted data to Pandas dataframe with gff3 columns"""
 
-    df = pd.DataFrame()
-    # gff3
-    # gff3_columns = Seqid	Source	Type	Start	End	Score	Strand	Phase	Attributes
-    # Create Dataframe, then read in data in order to fill the columns.
-    # So, for each dictionary in the list of dictionaries, add for each col
-    # Col (1) Accession (as Seqid)
-    # Col (2) bprom (as Source)
-    # Col (3) promoter (but this might be incorrect)
-    # Col (4) start and (5) end positions
-    # Col (6) LDF Score (Score)
-    # Col (7) strand_direction, '+' or '-' (strand)
-    # Col (8) phase = '.'
-    # Col (9) (attributes)
-
-    return
+    df = pd.DataFrame(extracted_data,
+                      columns=['seqid', 'source', 'type', 'start', 'end',
+                               'score', 'strand', 'phase', 'attributes']
+                      )
+    return df
 
 
 def write_to_gff3(dataframe):
@@ -241,10 +248,11 @@ if __name__ == '__main__':
 
     # print(remove_promoterless_features(concatenated_bprom_file)[2])
     # print('working_file: ', working_file[0])
-    print(' position:', position,
-          '\n', '-10 promoter:', promoters['minus10_pos'], promoters['minus10_seq'],
-          '\n', '-35 promoter:', promoters['minus35_pos'], promoters['minus35_seq'],
-          '\n', 'strand direction: ', strand_direction,
-          '\n', 'accession: ', accession,
-          )
-    print(extract_data_for_all_features(working_file))
+    # print(' position:', position,
+    #       '\n', '-10 promoter:', promoters['minus10_pos'], promoters['minus10_seq'],
+    #       '\n', '-35 promoter:', promoters['minus35_pos'], promoters['minus35_seq'],
+    #       '\n', 'strand direction: ', strand_direction,
+    #       '\n', 'accession: ', accession,
+    #       )
+    print(extract_data_for_all_features(working_file)),
+    print(convert_to_dataframe(extract_data_for_all_features(working_file)))
